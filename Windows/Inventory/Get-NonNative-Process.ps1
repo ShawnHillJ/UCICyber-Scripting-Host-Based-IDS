08c01f79966ca011fc8458c79461c6e546162dd8
ShawnHillJ <shawnhillj@gmail.com>
2019-09-04 12:24:16 -0700

    added process scanner

---
+++ Windows/Inventory/Get-NonNative-Process.ps1
@@ -0,0 +1,61 @@
+﻿$filterList = @(
+    "ApplicationFrameHost";
+    "autochk";
+    "calc";
+    "chkdsk";
+    "csrss";
+    "dllhost";
+    "dwm";
+    "explorer";
+    "eventvwr";
+    "grpconv";
+    "Idle";
+    "iexplore";
+    "igfxCUIService";
+    "igfxEM";
+    "logonui";
+    "lsass";
+    "MicrosoftEdge";
+    "MicrosoftEdgeCP";
+    "MicrosoftEdgeSH";
+    "mmc";
+    "msconfig";
+    "msiexec";
+    "MsMpEng";
+    "mspaint";
+    "NisSrv";
+    "notepad";
+    "ntoskrnl";
+    "nvudisp";
+    "OneDrive";
+    "pdboot";
+    "Registry";
+    "regsvr32";
+    "rundll32";
+    "runonce";
+    "sc";
+    "schtasks";
+    "services";
+    "shutdown";
+    "sihost";
+    "smss";
+    "sndvol32";
+    "spoolsv";
+    "svchost";
+    "System";
+    "SystemSettings";
+    "taskhostw";
+    "taskmgr";
+    "TrustedInstaller";
+    "userinit";
+    "wininit";
+    "winlogon";
+    "WinStore.App";
+    "wmiPrvSE";
+    "wuauclt";
+    "wuauclt1";
+    "wupdmgr";
+)
+
+$nonNativeProcesses = Get-Process | Where-Object -Property ProcessName -NotIn $filterList
+$nonNativeProcesses

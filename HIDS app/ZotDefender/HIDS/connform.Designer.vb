4140f18a3fde4326d3799e729070f41b74a7d662
brandonshiyay <43565071+brandonshiyay@users.noreply.github.com>
2019-09-18 15:46:07 -0700

    Add files via upload

---
+++ HIDS app/ZotDefender/HIDS/connform.Designer.vb
@@ -0,0 +1,221 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class connform
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Status = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.logdate = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Source = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.srcport = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.destination = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.destport = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.state = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.pid = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(42, 442)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(81, 17)
+        Me.CheckBox1.TabIndex = 0
+        Me.CheckBox1.Text = "CheckBox1"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(52, 105)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(179, 342)
+        Me.ListBox1.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(42, 400)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(146, 400)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Status, Me.logdate, Me.Source, Me.srcport, Me.destination, Me.destport, Me.state, Me.pid})
+        Me.DataGridView1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.DataGridView1.Location = New System.Drawing.Point(0, 0)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(545, 342)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.DataGridView1)
+        Me.Panel1.Controls.Add(Me.ListBox1)
+        Me.Panel1.Location = New System.Drawing.Point(257, 43)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(545, 342)
+        Me.Panel1.TabIndex = 5
+        '
+        'Status
+        '
+        Me.Status.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells
+        Me.Status.HeaderText = "Status"
+        Me.Status.Name = "Status"
+        Me.Status.Width = 62
+        '
+        'logdate
+        '
+        Me.logdate.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells
+        Me.logdate.HeaderText = "Date"
+        Me.logdate.Name = "logdate"
+        Me.logdate.Width = 55
+        '
+        'Source
+        '
+        Me.Source.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells
+        Me.Source.HeaderText = "Source"
+        Me.Source.Name = "Source"
+        Me.Source.Width = 66
+        '
+        'srcport
+        '
+        Me.srcport.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells
+        Me.srcport.HeaderText = "Port"
+        Me.srcport.Name = "srcport"
+        Me.srcport.Width = 51
+        '
+        'destination
+        '
+        Me.destination.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells
+        Me.destination.HeaderText = "Destination"
+        Me.destination.Name = "destination"
+        Me.destination.Width = 85
+        '
+        'destport
+        '
+        Me.destport.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells
+        Me.destport.HeaderText = "Port"
+        Me.destport.Name = "destport"
+        Me.destport.Width = 51
+        '
+        'state
+        '
+        Me.state.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells
+        Me.state.HeaderText = "State"
+        Me.state.Name = "state"
+        Me.state.Width = 57
+        '
+        'pid
+        '
+        Me.pid.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells
+        Me.pid.HeaderText = "PID"
+        Me.pid.Name = "pid"
+        Me.pid.Width = 50
+        '
+        'Panel2
+        '
+        Me.Panel2.Controls.Add(Me.TextBox1)
+        Me.Panel2.Controls.Add(Me.ListBox2)
+        Me.Panel2.Location = New System.Drawing.Point(42, 43)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(179, 342)
+        Me.Panel2.TabIndex = 6
+        '
+        'ListBox2
+        '
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.Location = New System.Drawing.Point(0, 23)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(179, 316)
+        Me.ListBox2.TabIndex = 0
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(0, 0)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(179, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(846, 530)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Status As DataGridViewTextBoxColumn
+    Friend WithEvents logdate As DataGridViewTextBoxColumn
+    Friend WithEvents Source As DataGridViewTextBoxColumn
+    Friend WithEvents srcport As DataGridViewTextBoxColumn
+    Friend WithEvents destination As DataGridViewTextBoxColumn
+    Friend WithEvents destport As DataGridViewTextBoxColumn
+    Friend WithEvents state As DataGridViewTextBoxColumn
+    Friend WithEvents pid As DataGridViewTextBoxColumn
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents ListBox2 As ListBox
+    Friend WithEvents TextBox1 As TextBox
+End Class
---
+++ HIDS app/ZotDefender/HIDS/connform.vb
@@ -0,0 +1,74 @@
+﻿Imports System.IO
+
+Public Class connform
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim table = DataGridView1
+        Dim list = ListBox2
+        Dim lfilep As String = "C:\Users\Admin\Desktop\log.txt"
+        Dim bfilep As String = "C:\Users\Admin\Desktop\list.txt"
+        Dim btitle = TextBox1
+        btitle.AppendText("Blocked Hosts")
+        btitle.ReadOnly = True
+        Dim addbtn = Button1
+        addbtn.Text = "Add"
+        Dim dltbtn = Button2
+        dltbtn.Text = "Delete"
+
+        For Each line As String In File.ReadAllLines(lfilep)
+            Dim data = line.Split(",")
+            Dim src As String = data(0)
+            Dim sport As String = data(1)
+            Dim dest As String = data(2)
+            Dim dport As String = data(3)
+            Dim state As String = data(4)
+            Dim conntype As String = data(5)
+            Dim pid As String = data(6)
+            Dim conndate As String = data(7)
+            Dim status As String = data(8)
+            table.Rows.Add(status, conndate, src, sport, dest, dport, state, pid)
+        Next
+        For Each host As String In File.ReadAllLines(bfilep)
+            list.Items.Add(host)
+
+        Next
+    End Sub
+
+    Private Sub TableLayoutPanel1_Paint(sender As Object, e As PaintEventArgs)
+
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim list = ListBox2
+        Dim table = DataGridView1
+        If (table.CurrentCell.ColumnIndex.Equals(2)) Or (table.CurrentCell.ColumnIndex.Equals(4)) Then
+
+            list.Items.Add(table.CurrentCell.Value)
+        End If
+
+    End Sub
+
+    Private Sub ListBox2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox2.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim list = ListBox2
+        list.Items.Remove(list.SelectedItem)
+    End Sub
+End Class
